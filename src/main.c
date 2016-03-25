fb49b80ba58b68c0b7e98dcb28f86fd7fa95ba9d
Christopher Frost <git@codewax.org>
2016-03-25 23:00:17 +0100

    Adding in broken ordinal calculator

--- src/main.c
+++ src/main.c
@@ -3,7 +3,20 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 static TextLayer *s_date_layer;
-static GFont s_font;
+static GFont s_time_font;
+static GFont s_date_font;
+
+static char get_suffix(int n)
+{
+  char ordinal[3];
+  switch (n) {
+    case 1: ordinal[0] = 's'; ordinal[1] = 't'; break;
+    case 2: ordinal[0] = 'n'; ordinal[1] = 'd'; break;
+    case 3: ordinal[0] = 'r'; ordinal[1] = 'd'; break;
+    default: ordinal[0] = 't'; ordinal[1] = 'h';
+  }
+  return *ordinal;
+}
 
 static void update_time() {
   // Get a tm structure
@@ -12,11 +25,14 @@ static void update_time() {
 
   // Write the current hours and minutes into a buffer
   static char s_time_buffer[8];
-  strftime(s_time_buffer, sizeof(s_time_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
+  strftime(s_time_buffer, sizeof(s_time_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M%P", tick_time);
 
   // Write the current hours and minutes into a buffer
-  static char s_date_buffer[8];
-  strftime(s_date_buffer, sizeof(s_date_buffer), "%a %b %d", tick_time);
+  static char s_date_buffer[13];
+//   get_suffix(tick_time.tm_mday);
+//   printf("BOOM %d", (*tick_time).tm_mday);
+  
+  strftime(s_date_buffer, sizeof(s_date_buffer), "%a %d" + get_suffix((*tick_time).tm_mday) + " %b", tick_time);
   
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_time_buffer);
@@ -32,20 +48,27 @@ static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
+  printf("Width %d, Height %d", bounds.size.w, bounds.size.h);
+  
+  //Chalk, round - 180 x 180
+  //Basalt, rectangular - 144 x 168
+  
   // Create the TextLayer with specific bounds
-  s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
-  s_date_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
+//   s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 25));
+  s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(10, 0), bounds.size.w, 50));
+  s_date_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(60, 50), bounds.size.w, 25));
 
   // Create GFont
-  s_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SOURCE_SANS_PRO_LIGHT_48));
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SOURCE_SANS_PRO_LIGHT_40));
+  s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SOURCE_SANS_PRO_LIGHT_20));
   
   // Improve the layout to be more like a watchface
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_background_color(s_date_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorWhite);
   text_layer_set_text_color(s_date_layer, GColorWhite);
-  text_layer_set_font(s_time_layer, s_font);
-  text_layer_set_font(s_date_layer, s_font);
+  text_layer_set_font(s_time_layer, s_time_font);
+  text_layer_set_font(s_date_layer, s_date_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
 
@@ -58,7 +81,8 @@ static void main_window_unload(Window *window) {
   // Destroy TextLayers & GFont
   text_layer_destroy(s_time_layer);
   text_layer_destroy(s_date_layer);
-  fonts_unload_custom_font(s_font);
+  fonts_unload_custom_font(s_time_font);
+  fonts_unload_custom_font(s_date_font);
 }
 
 static void init() {
