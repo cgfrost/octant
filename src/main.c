eebaf97c2fcd82a3efecb9ff27915aeb3e09f36b
Christopher Frost <git@codewax.org>
2016-03-26 22:31:26 +0100

    Fixed ordinal code

--- src/main.c
+++ src/main.c
@@ -3,20 +3,7 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 static TextLayer *s_date_layer;
-static GFont s_time_font;
-static GFont s_date_font;
-
-static char get_suffix(int n)
-{
-  char ordinal[3];
-  switch (n) {
-    case 1: ordinal[0] = 's'; ordinal[1] = 't'; break;
-    case 2: ordinal[0] = 'n'; ordinal[1] = 'd'; break;
-    case 3: ordinal[0] = 'r'; ordinal[1] = 'd'; break;
-    default: ordinal[0] = 't'; ordinal[1] = 'h';
-  }
-  return *ordinal;
-}
+static GFont s_time_font, s_date_font;
 
 static void update_time() {
   // Get a tm structure
@@ -27,12 +14,23 @@ static void update_time() {
   static char s_time_buffer[8];
   strftime(s_time_buffer, sizeof(s_time_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M%P", tick_time);
 
-  // Write the current hours and minutes into a buffer
+  // Write the current date into a buffer
   static char s_date_buffer[13];
-//   get_suffix(tick_time.tm_mday);
-//   printf("BOOM %d", (*tick_time).tm_mday);
   
-  strftime(s_date_buffer, sizeof(s_date_buffer), "%a %d" + get_suffix((*tick_time).tm_mday) + " %b", tick_time);
+  static char template[] = "%a %d-- %b";
+  
+  switch ((*tick_time).tm_mday) {
+    case 1: template[5] = 's'; template[6] = 't'; break;
+    case 21: template[5] = 's'; template[6] = 't'; break;
+    case 31: template[5] = 's'; template[6] = 't'; break;
+    case 2: template[5] = 'n'; template[6] = 'd'; break;
+    case 22: template[5] = 'n'; template[6] = 'd'; break;
+    case 3: template[5] = 'r'; template[6] = 'd'; break;
+    case 23: template[5] = 'r'; template[6] = 'd'; break;
+    default: template[5] = 't'; template[6] = 'h';
+  }
+        
+  strftime(s_date_buffer, sizeof(s_date_buffer), template, tick_time);
   
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_time_buffer);
@@ -47,20 +45,17 @@ static void main_window_load(Window *window) {
   // Get information about the Window
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
-
-  printf("Width %d, Height %d", bounds.size.w, bounds.size.h);
   
   //Chalk, round - 180 x 180
   //Basalt, rectangular - 144 x 168
   
   // Create the TextLayer with specific bounds
-//   s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 25));
   s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(10, 0), bounds.size.w, 50));
   s_date_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(60, 50), bounds.size.w, 25));
 
   // Create GFont
   s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SOURCE_SANS_PRO_LIGHT_40));
-  s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SOURCE_SANS_PRO_LIGHT_20));
+  s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SOURCE_SANS_PRO_LIGHT_22));
   
   // Improve the layout to be more like a watchface
   text_layer_set_background_color(s_time_layer, GColorClear);
